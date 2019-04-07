c14936eec9b37f82187eda5aca31316e40247574
AnneManzhura <manzhurik@gmail.com>
2019-04-07 13:09:04 +0300

    Update api_password.js

--- backend/api_password.js
+++ backend/api_password.js
@@ -1,5 +1,75 @@
 
 var crypto = require('crypto');
+var nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
+var json = require('./data/kindergarten-data.json');
+var generatePassword = require('password-generator');
+
+
+
+
+
+exports.generatePassword = function(email)
+{
+    var password = generatePassword();
+    var hashdata = saltHashPassword1(password);
+    var salt = hashdata.salt;
+    var hash =hashdata.hash;
+
+    console.log("PASSWORD   "+password);
+
+    var ac={user: json.kindergarten.email.user, pass: json.kindergarten.email.pass};
+    var par={from: "manzhurik@gmail.com", to: [email], subject:"Password for KinderGarten", text: password}
+
+    sendEmail(ac, par);
+
+
+    var res ={hash: hash, salt: salt};
+    return res;
+};
+
+
+
+
+function sendEmail(account, params) {
+
+
+    console.log(account.user);
+    console.log(account.pass);
+
+    var smtpTransport1 = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    }));
+
+    var toEmail = params.to[0];
+    for (var i = 1; i < params.to.length; i++) {
+        toEmail += ', ' + params.to[i];
+    }
+
+    // setup email data with unicode symbols
+    var mailOptions = {
+        from: params.from, // sender address
+        to: toEmail, // list of receivers
+        subject: params.subject, // Subject line
+        text: params.text
+    };
+
+    // send mail with defined transport object
+    smtpTransport1.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log('Error while sending mail: ' + error);
+        } else {
+            console.log('Message sent: %s', info.messageId);
+        }
+        smtpTransport1.close(); // shut down the connection pool, no more messages.
+    });
+}
+
 
 var genRandomString = function(length){
     return crypto.randomBytes(Math.ceil(length/2))
@@ -39,8 +109,16 @@ exports.saltHashPassword = function(userpassword)
     var res ={hash: passwordData.passwordHash, salt:passwordData.salt}
 
     return res;
-    //console.log('UserPassword = '+userpassword);
-    //console.log('Passwordhash = '+passwordData.passwordHash);
-    //console.log('nSalt = '+passwordData.salt);
 };
+
+function saltHashPassword1(userpassword)
+{
+    var salt = genRandomString(16); /** Gives us salt of length 16 */
+var passwordData = sha512(userpassword, salt);
+
+    var res ={hash: passwordData.passwordHash, salt:passwordData.salt}
+
+    return res;
+};
+
 
