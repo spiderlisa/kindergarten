f078dad0299218da94c41400ad391b99150fc4e9
AnneManzhura <manzhurik@gmail.com>
2019-04-07 13:09:49 +0300

    Update admin-routes.js

--- backend/routes/admin-routes.js
+++ backend/routes/admin-routes.js
@@ -7,4 +7,8 @@ router.get('/register-child', api.renderRegChildPage);
 router.get('/register-parent', api.renderRegParentPage);
 router.get('/register-teacher', api.renderRegTeacherPage);
 
+router.post('/registerChild', api.registerChild);
+router.post('/registerParent', api.registerParent);
+router.post('/registerTeacher', api.registerTeacher);
+
 module.exports = router;
